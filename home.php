1031465951df3f6a9f63d093eb191d82ebd48fa2
Romero6208 <147239683+Romero6208@users.noreply.github.com>
2024-04-20 15:06:32 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,10 @@
+<div class="card">
+           <div class="card-body">
+              <p>
+                <img src="gambar/webdev.jpeg" class="rounded float-end"
+                alt="WebDev">
+                <b><i>Web Programming</b></i> adalah istilah yang luas untuk pekerjaan yang terlibat dalam mengembangkan suatu situs web untuk internet atau intranet.
+                <br>
+              </p>
+           </div>
+       </div>
